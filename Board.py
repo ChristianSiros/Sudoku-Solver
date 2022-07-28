06e64c75aac16e3bc49cd4df8b7d72bfeeca6825
Christian Siros <73655323+Cbsiros@users.noreply.github.com>
2022-07-27 22:13:32 -0400

    Added Cell Class
    Added a class named Cell, in preparation for future optimizations.

--- Board.py
+++ Board.py
@@ -1,18 +1,20 @@
 import math
+from Cell import Cell
 
 class Board:
 
     def __init__(self):
-        self.grid = [[0 for i in range(9)] for j in range(9)]     
-        self.found_solution = False
+        self.grid = [[Cell(row, col) for col in range(9)] for row in range(9)]     
 
     def set_grid(self, grid):
-        self.grid = grid
+        for row in range(9):
+            for col in range(9):
+                self.grid[row][col].set_value(grid[row][col])
     
     def print(self):
-        for i in range(9):
-            for j in range(9):
-                print(self.grid[i][j], " ", end = "")
+        for row in range(9):
+            for col in range(9):
+                print(self.grid[row][col].get_value(), " ", end = "")
             print()
         print()
 
@@ -21,18 +23,18 @@ class Board:
         col = index % 9
 
         for x in range(9):
-            if self.grid[row][x] == num:
+            if self.grid[row][x].get_value() == num:
                 return False
 
         for x in range(9):
-            if self.grid[x][col] == num:
+            if self.grid[x][col].get_value() == num:
                 return False
     
         startRow = row - row % 3
         startCol = col - col % 3
         for i in range(3):
             for j in range(3):
-                if self.grid[i + startRow][j + startCol] == num:
+                if self.grid[i + startRow][j + startCol].get_value() == num:
                     return False
         return True
  
@@ -40,20 +42,20 @@ class Board:
         row = math.floor(index/9)
         col = index % 9
 
-        if (index == 80):
+        if (index == 81):
             return True
 
-        if self.grid[row][col] > 0:
+        if (self.grid[row][col].get_value() > 0):
             return self.solve_by_backtracking(index + 1)
 
         for num in range(1, 10):
         
             if self.is_valid(index, num):
             
-                self.grid[row][col] = num
+                self.grid[row][col].set_value(num)
 
                 if self.solve_by_backtracking(index + 1):
                     return True
     
-            self.grid[row][col] = 0
+            self.grid[row][col].set_value(0)
         return False
---
+++ Cell.py
@@ -0,0 +1,19 @@
+class Cell:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.value = 0
+        self.candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.num_candidates = len(self.candidates)
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+    
+    def get_candidates(self):
+        return self.candidates
+    
+    def get_num_candidates(self):
+        return self.num_candidates
